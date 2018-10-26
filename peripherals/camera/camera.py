b9f2af4b8d07d12fe7e74038e3afe2b62ba98c42
Marc Rossi <14182660+forzarossi@users.noreply.github.com>
2018-10-26 12:40:29 -0400

    Update camera.py

--- peripherals/camera/camera.py
+++ peripherals/camera/camera.py
@@ -1,22 +1,40 @@
+############################################################
+# file: camera.py                                          #
+# author: Marc Rossi                                       #
+# email: marcrossi@umass.edu                               #
+############################################################
+
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
 from PIL import Image
 import numpy as np
 
+##############################
+# Takes image using PiCamera #
+##############################
 stream = BytesIO()
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
 camera.capture(stream, format='jpeg')
+
+##############################
+# Converts image to np.array #
+##############################
 stream.seek(0)
 image = Image.open(stream)
-image_array = numpy.asarray(image)
+image_array = np.asarray(image)
 
-zeros = np.zeros((100, 100), dtype=np.uint8)
-zeros[:5,:5] = 255
+######################################################
+# check where the pixel is equal to 255(black)       #
+# TODO: Check where values > some threshold of black #
+######################################################
+indices = np.where(image_array == [255])
 
-indices = np.where(zeros == [255])
+##########################################################
+# Records indicies and the coordinates of those indicies #
+##########################################################
 print indices
 coordinates = zip(indices[0], indices[1])
 print coordinates
