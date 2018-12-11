1974d9d4098005b88807f7640014e252ec92de1b
Marc Rossi <14182660+forzarossi@users.noreply.github.com>
2018-12-11 14:02:49 -0500

    Update color_detect.py

--- peripherals/camera/color_detect.py
+++ peripherals/camera/color_detect.py
@@ -29,24 +29,26 @@ def take_image():
 
 def process_image(image):
     image_array = np.asarray(image)
-    #find_black_indicies(image_array)
-    #find_yellow_indicies(image_array)
-
-    cropped_image = image.crop(((0, 400, 640,480)))
+    cropped_image = image.crop(((0, 350, 640, 480)))
     cropped_array = np.asarray(cropped_image)
-    if(len(find_red_indicies(cropped_array)[0]) > 40000):
+    
+    if(len(find_green_indicies(cropped_array)[0]) > 850):
+        print("Go")
+        ser = serial.Serial('/dev/ttyACM0')
+        ser.write(b'1')
+    elif(len(find_red_indicies(cropped_array)[0]) > 40000):
+        print("Stop")
         ser = serial.Serial('/dev/ttyACM0')
         ser.write(b'stop')
-
-    #to_filtered_image(cropped_array, "red")
-    # to_filtered_image(image_array, "black")
-    # to_filtered_image(image_array, "yellow")
 
 ########################################################################
 #    Check where the pixel is equal to 0,0,0 with a threshold of 50    #
 ########################################################################
+def find_green_indicies(image_array):
+    return np.where(np.all(np.logical_and(image_array > (200, 200, 200), image_array < (250, 255, 250)) == True, axis=-1))
+
 def find_black_indicies(image_array):
-    return np.where(np.all(np.logical_and(image_array >(0,0,0), image_array < (50,50,50))==True, axis=-1))
+    return np.where(np.all(np.logical_and(image_array >(0,0,0), image_array < (50,50,50)) == True, axis=-1))
 
 def find_red_indicies(image_array):
     return np.where(np.all(np.logical_and(image_array > (150, 50, 50), image_array < (255, 150, 150)) == True, axis=-1))
@@ -81,7 +83,7 @@ def to_filtered_image(image_array, color):
 
         Image.fromarray(rgba).save(color + "isolated.png")
 
-take_image()
+#take_image()
 
 
 
